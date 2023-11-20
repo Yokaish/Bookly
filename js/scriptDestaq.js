f87c0f78369c8968977765fe9054140deb0b0736
Yokaish <138620582+Yokaish@users.noreply.github.com>
2023-11-20 19:09:23 -0300

    Initial commit

---
+++ js/scriptDestaq.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".slide__content__dest", {
+    slidesPerView: 1,
+    centerSlide: "true",
+    fade: "true",
+    spaceBetween: 25,
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  });
